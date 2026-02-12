4eae1ffff99e49858c18f0d330a387c949edbee2
xyzsince2014 <xyzsince2014@gmail.com>
2026-02-12 23:58:32 +0900

    changed DB_HOST, AUTH_CONTAINER, and ip

--- docker-run.sh
+++ docker-run.sh
@@ -3,5 +3,5 @@ docker container run -d --rm \
   --env-file $(pwd)/dev.env \
   --name tokyomap-resource \
   --net network_tokyomap \
-  --ip 192.168.56.130 \
+  --ip 172.20.0.130 \
   tokyomap.resource:dev 
